ae07d678409d7aff984e135781141aa53cdfbac0
Caleb Ford <caleb.ford.sto@gmail.com>
2024-07-09 16:02:36 -0500

    working commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,44 @@
-import { useState } from "react";
-import { addNewJoke } from "./services/jokeService.jsx";
+import { useEffect, useState } from "react";
+import { addNewJoke, getAllJokes } from "./services/jokeService.jsx";
 import stevePic from "./assets/steve.png";
 export const App = () => {
   const [newJoke, setNewJoke] = useState("");
+  const [allJokes, setAllJokes] = useState([]);
+  const [untoldJokes, setUntoldJokes] = useState([]);
+  const [toldJokes, setToldJokes] = useState([]);
+  // const [render, setRender] = useState(0);
+
+  useEffect(() => {
+    updateJokes();
+  }, []);
+
+  useEffect(() => {
+    const jokesToTell = allJokes.filter((joke) => !joke.told);
+    setUntoldJokes(jokesToTell);
+  }, [allJokes]);
+
+  useEffect(() => {
+    const jokesTold = allJokes.filter((joke) => joke.told);
+    setToldJokes(jokesTold);
+  }, [allJokes]);
+
+  const updateJokes = () => {
+    getAllJokes().then((jokes) => setAllJokes(jokes));
+  };
+
+  const handleSubmit = async () => {
+    const jokeObject = {
+      text: newJoke,
+      told: false,
+    };
+    await addNewJoke(jokeObject);
+    setNewJoke("");
+    updateJokes();
+  };
+  const moveJoke = (event) => {
+    console.log(event.currentTarget.value);
+  };
+
   return (
     <div className="app-container">
       <div className="app-heading">
@@ -25,17 +61,43 @@ export const App = () => {
         <button
           className="joke-input-submit"
           type="submit"
-          onClick={(click) => {
-            const jokeObject = {
-              text: newJoke,
-              told: false,
-            };
-            addNewJoke(jokeObject);
-            setNewJoke("");
-          }}
+          onClick={handleSubmit}
         >
           ADD
         </button>
+      </div>
+      <div className="joke-lists-container">
+        <div className="joke-list-container">
+          <h2>
+            Untold Jokes
+            <span className="untold-count">{untoldJokes.length}</span>
+          </h2>
+          <ul>
+            {untoldJokes.map((joke) => (
+              <li key={joke.id} className="joke-list-item">
+                <p className="joke-list-item-text">{joke.text}</p>
+                <button value={joke} title="Move to Told" onClick={moveJoke}>
+                  <i className="fa-solid fa-arrow-right" />
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="joke-list-container">
+          <h2>
+            Told Jokes<span className="told-count">{toldJokes.length}</span>
+          </h2>
+          <ul>
+            {toldJokes.map((joke) => (
+              <li key={joke.id} className="joke-list-item">
+                <p className="joke-list-item-text">{joke.text}</p>
+                <button title="Move to Untold">
+                  <i className="fa-solid fa-arrow-left"></i>
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/services/jokeService.jsx
+++ src/services/jokeService.jsx
@@ -1,5 +1,9 @@
-export const addNewJoke = (jokeObject) => {
-  const response = fetch("http://localhost:8088/jokes", {
+export const getAllJokes = () => {
+  return fetch("http://localhost:8088/jokes").then((res) => res.json());
+};
+
+export const addNewJoke = async (jokeObject) => {
+  return await fetch("http://localhost:8088/jokes", {
     method: "POST",
     headers: {
       "content-type": "application/json",
